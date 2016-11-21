367b5da4aeba5cc82d63024eff164921d3df914e
Brittany Chiu <bcc5160@psu.edu>
2016-11-20 20:50:12 -0500

    update

--- tattle_dbInterface.py
+++ tattle_dbInterface.py
@@ -18,6 +18,7 @@ ERR_ALREADY_EXISTS = -2
 class Tattle_dbInterface:
     # Variables
     database_file = "tattle_users.json"
+    current_users = []
 
     # Initialize the constructor for this class
     def __init__(self):
@@ -28,22 +29,26 @@ class Tattle_dbInterface:
         db = TinyDB(database_file)
         query = Query()
 
-        results = db.search( (query.chatid == chatid) && (query.username == username) )
-
-        if(results == []):
-            return False
-        else
+        # result is an array of usernames of this group chat
+        result = db.search( (query.chatid == chatid) && (query.usernames == usernames) )
+        usernames = result[0]["usernames"]
+        
+        # Check if username is in the list
+        if username in usernames:
             return True
+        else:
+            return False
     
     # Add user to tattle's database
     def addUser(self, chatid, username):
         db = TinyDB(database_file)
+        query = Query()
 
         # Error Handling : Check if user is already added
         if self.CheckUserExists(chaid, username) == True:
             return ERR_ALREADY_EXISTS
-
-        new_elementID = db.insert( {'chatid': chatid, 'username': username} )
+        
+        chat_usernames = db.insert( {'chatid': chatid, 'username': username} )
 
         # Error handling : Check if successfully added
         if new_elementID == []:
@@ -59,14 +64,21 @@ class Tattle_dbInterface:
     def getUsersCount(self, chatid):
         db = TinyDB(database_file)
         query = Query()
-        # TODO: make insert add user an array to key of chatid
+
         results = db.search((query.chaid == chatid))
 
     # Pick a random user from the database of this current chat
-    def pickUser(self, chatid, username):
+    def pickUser(self, chatid):
+        db = TinyDB(database_file)
+        query = Query()
         current_time = datetime.now()
-        user_count = 
-        random_number = random.randint(0,)
-        
-        # TODO: Finish this
+
+        result = db.search(chatid == chatid)
+
+        users = result[0]["usernames"]
+
+        user_count = len(users) 
+        random_number = random.randint(0, user_count)
+        chosen_user = users[random_number]
+
         return chosen_user
